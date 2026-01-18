c5e480235e26908cfaf7746f737e09b3ae73e573
Zoltán Török <torokzoltan0348@gmail.com>
2026-01-18 17:17:30 +0200

    Updated service provider

--- src/FilamentDragAndScrollServiceProvider.php
+++ src/FilamentDragAndScrollServiceProvider.php
@@ -27,26 +27,23 @@ class FilamentDragAndScrollServiceProvider extends ServiceProvider
         $cssExt = $useMinified ? '.min.css' : '.css';
         $jsExt = $useMinified ? '.min.js' : '.js';
 
-        // Register assets
-        FilamentAsset::register([
-            Css::make('filament-drag-and-scroll-core-styles', __DIR__ . "/../resources/{$cssPath}/filament-drag-and-scroll{$cssExt}"),
-            Js::make('filament-drag-and-scroll-core-scripts', __DIR__ . "/../resources/{$jsPath}/filament-drag-and-scroll{$jsExt}"),
-        ], package: static::$name);
+        // Note: Core assets are not auto-registered with Filament
+        // They will only be published manually when needed
 
         // Publish assets
         if ($this->app->runningInConsole()) {
             // Publish minified assets to public directory without .min suffix
             $this->publishes([
-                __DIR__ . '/../resources/dist/css/filament-drag-and-scroll.min.css' => public_path('css/filament-drag-and-scroll.css'),
+                __DIR__ . '/../resources/dist/css/filament-drag-and-scroll.min.css' => public_path('css/filament-drag-and-scroll/filament-drag-and-scroll.css'),
             ], 'filament-drag-and-scroll-css');
 
             $this->publishes([
-                __DIR__ . '/../resources/dist/js/filament-drag-and-scroll.min.js' => public_path('js/filament-drag-and-scroll.js'),
+                __DIR__ . '/../resources/dist/js/filament-drag-and-scroll.min.js' => public_path('js/filament-drag-and-scroll/filament-drag-and-scroll.js'),
             ], 'filament-drag-and-scroll-js');
 
             $this->publishes([
-                __DIR__ . '/../resources/dist/css/filament-drag-and-scroll.min.css' => public_path('css/filament-drag-and-scroll.css'),
-                __DIR__ . '/../resources/dist/js/filament-drag-and-scroll.min.js' => public_path('js/filament-drag-and-scroll.js'),
+                __DIR__ . '/../resources/dist/css/filament-drag-and-scroll.min.css' => public_path('css/filament-drag-and-scroll/filament-drag-and-scroll.css'),
+                __DIR__ . '/../resources/dist/js/filament-drag-and-scroll.min.js' => public_path('js/filament-drag-and-scroll/filament-drag-and-scroll.js'),
             ], 'filament-drag-and-scroll-assets');
 
             // Publish core filament package files specifically (minified versions without .min suffix)
